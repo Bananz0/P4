2f20e9e2f36617e471394c013327a50b271d3bbe
glenm@XPS <glenmuthokaq@gmail.com>
2024-02-21 17:29:57 +0000

    2.1

--- P4/P4.cpp
+++ P4/P4.cpp
@@ -3,8 +3,13 @@
 	
 class Matrix {
 private:
-	std::vector<std::vector<double>> values;
 	size_t rows, columns;
+	std::vector<std::vector<double>> data;
+public:	
+	int glenm;
+	Matrix(int rowsInput, int colsInput): rows(rowsInput) , columns(colsInput) {		
+		data.resize(rows, std::vector<double>(columns, 0));
+	}
 };
 
 int main()
